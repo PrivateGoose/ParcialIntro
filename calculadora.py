487a3e925a9e94a82f6b517fde0dd897c46e895d
Juan Pablo Perez <juan.perez.2019@upb.edu.co>
2019-05-21 18:15:38 -0500

    Mi cuarto commit

--- calculadora.py
+++ calculadora.py
@@ -42,7 +42,7 @@ num_decimal=int(input("Digite su número: "))
 var=decimal_octal(num_decimal)
 print(*var)
 
-def decimal_hexadecimal(num_decimal):
+'''def decimal_hexadecimal(num_decimal):
     modulos=[]
     
     while True:
@@ -77,13 +77,31 @@ def decimal_hexadecimal(num_decimal):
 
         num_convert.append(i)
 
-    return num_convert
+    return num_convert'''
+
+
+num_decimal=input("Digite su número: ") 
+'''var=decimal_hexadecimal(num_decimal)
+print(*var)'''
 
 
 
-num_decimal=int(input("Digite su número: ")) 
-var=decimal_hexadecimal(num_decimal)
-print(*var)
+
+
+def binario_decimal(num_binario):
+
+    string=str(num_binario)
+    
+    potencias=[]
+    for i in range(0,len(string)):
+        potencias.append(2**i)
+        potencias_reverse=[]
+        for a in reversed(potencias):
+            potencias_reverse.append(a)
+    return potencias_reverse
+
+print(binario_decimal(num_decimal))
+    
 
 '''x=int(input(print("Bienvenido, digite la opción que desee: \n 1)Traducir de decimal a binario \n 2)Traducir de decimal a octal \n 3)Traducir de decimal a hexadecimal \n 4)Traducir de binario a decimal \n 5)Traducir de binario a octal \n 6)Traducir de binario a hexadecimal \n 7)Traducir de octal a decimal \n 8)Traducir de octal a binario \n 9)Traducir de octal a hexadecimal \n 10)Traducir de hexadecimal a binario \n 11)Traducir de hexadecimal a decimal \n 12)Traducir de hexadecimal a octal")))
 if x==1:
